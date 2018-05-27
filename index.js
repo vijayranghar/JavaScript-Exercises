89dcafdd7b2667959c3a9382eb088c1e42c80e5f
VijayRanghar <vijayranghar@gmail.com>
2018-05-27 17:23:33 +0530

    fix question rendering logic

--- index.js
+++ index.js
@@ -26,45 +26,75 @@
       {label:'d',option:'Dolor sit amet, consectetur adipisicing elit ?'},
     ],
     correctAnswer: '1',
-  },]
-
-  // render first question
-  function renderInitialQuestion () {
-    nextButton.classList.toggle('is-visible')
-    renderQuestion(counter)
-  }
-
+  },{
+    question: '4 Orem ipsum dolor sit amet, consectetur adipisicing elit ?',
+    answer: [
+      {label:'a',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'b',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'c',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'d',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+    ],
+    correctAnswer: '1',
+  },
+  {
+    question: '5 Orem ipsum dolor sit amet, consectetur adipisicing elit ?',
+    answer: [
+      {label:'a',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'b',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'c',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+      {label:'d',option:'Dolor sit amet, consectetur adipisicing elit ?'},
+    ],
+    correctAnswer: '1',
+  },
+]
 
   function renderQuestion(questionNumber) {
     let optionsElement = document.getElementsByClassName("option")
     let progressHeading = document.getElementById("progress")
+    let progressBar = document.querySelector(".progress-bar")
 
-    progress = ((questionNumber+1)/questionsList.length) * 100
-    progressHeading.innerHTML=`${progress.toFixed()}%`
+    nextButton.classList="is-visible"
+    submitButton.classList="is-hidden"
 
-    let answer=[]
-    let question=[]
-    question.push(`<h4>${questionsList[questionNumber].question}</h4>
-      <h6>QUESTION 0 OF 2</h6>`)
-    questionsList[questionNumber].answer.forEach((item,index) => {
-      answer.push(`<label class="option"><div class="input-radio">
-      <span>${item.label}</span><input value=${item.option} name=${questionNumber} type="radio"/></div>
-      <div class="option-item">${item.option}</div></label>`)
-    })
-    questionsElement.innerHTML = `<li>${[...question,...answer].join('')}</li>`
+    console.log(counter, "inside render que")
+    if(questionNumber===0) {
+      previousButton.classList+= "is-disable"
+    }
+    else {
+      previousButton.classList.remove("is-disable")
+    }
+    if(questionNumber < questionsList.length) {
+      progress = ((questionNumber+1)/questionsList.length) * 100
+      progressHeading.innerHTML=`${progress.toFixed()}%`
+      progressBar.style.width=`${progress.toFixed()}%`
+      let answer=[]
+      let question=[]
+      question.push(`<h4>${questionsList[questionNumber].question}</h4>
+        <h6>QUESTION ${questionNumber+1} OF ${questionsList.length}</h6>`)
+      questionsList[questionNumber].answer.forEach((item,index) => {
+        answer.push(`<label class="option"><div class="input-radio">
+        <span>${item.label}</span><input value=${item.option} name=${questionNumber} type="radio"/></div>
+        <div class="option-item">${item.option}</div></label>`)
+      })
+      questionsElement.innerHTML = `<li>${[...question,...answer].join('')}</li>`
+    }
+    else {
+      questionsElement.innerHTML= "SUBMIT"
+      nextButton.classList="is-hidden"
+      submitButton.classList="is-visible"
+    }
   }
 
   //render next question
   function showNextQuestion() {
-    if(counter < questionsList.length-1) {
-      counter += 1
-    }
-    else {
-      nextButton.className+="is-hidden"
-      submitButton.classList.toggle('is-visible')
-      questionsElement.innerHTML='<li>finished</li>'
-      return
-    }
+    counter += 1
+    console.log(counter, "inside show next")
+    renderQuestion(counter)
+  }
+
+  //render Previous question
+  function showPreviousQuestion() {
+    counter -= 1
     renderQuestion(counter)
   }
 
@@ -75,7 +105,7 @@
     answers.forEach((item,index) => {
       score += (item.value === questionsList[index].correctAnswer) ? 1 : 0
     })
-    console.log(answers)
+    alert("score is = ",score)
   }
 
   function highlightSelected(e) {
@@ -105,13 +135,14 @@
   let progress = 0
   let submitButton = document.getElementById("submitButton")
   let nextButton = document.getElementById("nextButton")
+  let previousButton = document.getElementById("previousButton")
   let questionsElement = document.getElementById("questions")
-  let progressBar =  document.getElementById("progress")
 
 
   //startTestButton.addEventListener('click', renderInitialQuestion)
-  window.onLoad = renderInitialQuestion()
+  window.onLoad = renderQuestion(counter)
   submitButton.addEventListener('click', showResult)
   nextButton.addEventListener('click', showNextQuestion)
+  previousButton.addEventListener('click', showPreviousQuestion)
   questionsElement.addEventListener("click", highlightSelected)
 })()
